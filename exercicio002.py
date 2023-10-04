b150f45961978cbeeb8b5bed3a1c5eb2a9497c8f
Igor123456890 <133661518+Igor123456890@users.noreply.github.com>
2023-10-04 18:44:24 -0300

    exercicio002.py

---
+++ exercicio002.py
@@ -0,0 +1,19 @@
+"""
+Exercícios
+
+1) Mude a cor da linha
+2) Mude o formato da tartaruga
+3) Mude o tamanho do quadrado
+"""
+import turtle
+
+t = turtle.Turtle()
+t.shape('triangle') # Mudando o formato da tartaruga para um triângulo
+t.color('blue')
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
